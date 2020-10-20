a07bfb5fbdc8ae1b4cfa07f550e1df3bbbdc599b
Rianna Cleary <riannacleary@Rianna-Clearys-MacBook-Pro.local>
2020-10-20 11:40:03 -0400

    installed mocha

--- test/add.spec.js
+++ test/add.spec.js
@@ -1,10 +1,30 @@
 const add = require('../add')
+const assert = require('assert')
 
-describe('The add function', () => {
-    it('adds two numbers', () => {
-        const actual = add(1, 3)
+// const result = add(1,3)
+// const expected = 4;
+
+// an assertion is a check to see if our tests would pass or not. 
+// usually you would have an expected to compare to the result.
+// assertion:
+// if (result === 4) {
+//     console.log('tests pass!')
+// } else {
+//     throw new Error('Expected 1 + 3 to equal 4')
+// }
+
+// assert.strictEqual(result, expected, 'Add function does not work properly.')
+
+// provided by mocha
+describe('The add fucntion', () => {
+    it ('adds two numbers', () => {
+        const result = add(1, 3)
         const expected = 4
 
-        expect(actual).toBe(expected)
+        assert.strictEqual(result, expected)
+
     })
 })
+
+
+// test comment 
